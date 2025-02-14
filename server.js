c9fb8e54ee6eb19e4a783f31e8d72094e0d91875
User Name <user@email.com>
2025-02-14 16:22:45 -0500

    fruit show page

--- server.js
+++ server.js
@@ -18,17 +18,37 @@ app.use(express.urlencoded({extended: false}));
 
 
 
-
+//get route to landing page i think
 app.get('/', async (req,res) => {
     res.render('index.ejs');
 });
 
 
+//GET to /fruits * use a res.send to the page to make sure the connection works before res.render
+app.get('/fruits', async (req,res) =>{
+    const allFruits = await Fruit.findById(); //this route when requested gets all the fruits now
+    // console.log(allFruits) //this is to check in the console if it does
+   res.render('fruits/index.ejs', {fruits: allFruits})
+})
+
+
+
+
+
+//Get to /create a new fruit page with the submit button
 app.get('/fruits/new', (req,res) => {
     res.render('fruits/new.ejs');
 });
 
+//GET route to fruitID
+app.get('/fruits/:fruitId',async (req,res) => {
+    const foundFruit = await Fruit.findById(req.params.fruitId);
+    res.render('/fruits/show.ejs', {fruit:foundFruit});
+});
 
+
+
+//submit posts the req.body 
 app.post('/fruits', async (req,res) => {
     if (req.body.isReadyToEat === 'on'){
         req.body.isReadyToEat = true;
@@ -36,7 +56,7 @@ app.post('/fruits', async (req,res) => {
         req.body.isReadyToEat = false;
     }
     await Fruit.create(req.body);
-    res.redirect('/fruits/new')
+    res.redirect('/fruits')
 })
 
 
